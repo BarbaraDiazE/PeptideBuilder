88e6ec5d28b52b3e43846f1aaa38096493704e12
Barbara <debi_1223@hotmail.com>
2020-01-16 12:20:26 -0600

    create Build app

--- PeptideBuilder/settings.py
+++ PeptideBuilder/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES = os.path.join(BASE_DIR+"/templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Build'
 ]
 
 MIDDLEWARE = [
